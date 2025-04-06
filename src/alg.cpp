97d114283039e4633903a66b12ec4899679f6197
HntgerL <lbedevd@gmail.com>
2025-04-06 23:01:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -27,32 +27,34 @@ int countPairs2(int *arr, int len, int value) {
   }
 return count;
 }
-int countPairs3(int *arr, int len, int value) {
-  int count = 0;
-  int middle = 0;
-  int lboard = 0;
-  int rboard = 0;
-  int find_num = 0;
-  int flag = 0;
-  for (int i = 0; i < len-1; ++i) {
-    find_num = value - arr[i];
-    flag = 0;
-    lboard = 0;
-    rboard = len - 1;
-    while (lboard <= rboard && flag == 0) {
-      middle = (lboard + rboard) / 2;
-      if (arr[middle] > find_num) {
-        rboard = middle - 1;
-      } else if (arr[middle] < find_num) {
-        lboard = middle + 1;
-      } else {
-        if (arr[middle] != value) {
-          count += 1;
+int binarSearc(int* arr, int left, int right, int target, bool find_first) {
+    int result = -1;
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (arr[mid] == target) {
+        result = mid;
+        if (find_first) {
+          right = mid - 1;
+        } else {
+          left = mid + 1;
         }
-        arr[middle] = value;
-        flag = 1;
+      } else if (arr[mid] < target) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
       }
     }
-  }
-return count;
+    return result;
+}
+int countPairs3(int *arr, int len, int value) {
+    int count = 0;
+    for (int i = 0; i < len; ++i) {
+      int complement = value - arr[i];
+      int first = binarSearc(arr, i + 1, len - 1, complement, true);
+      if (first != -1) {
+        int last = binarSearc(arr, first, len - 1, complement, false);
+        count += last - first + 1;
+      }
+    }
+    return count;
 }
